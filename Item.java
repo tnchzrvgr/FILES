4eb2cb91778fdaad7509e9a53769338f9137d55c
tnchzrvgr <vergaratoni783@gmail.com>
2025-11-21 20:46:29 +0800

    Update Item.java

--- Item.java
+++ Item.java
@@ -3,4 +3,16 @@ public class Item extends Equipment {
     public Item(int id, String name, String category, int quantity, String condition) {
         super(id, name, category, quantity, condition);
     }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("\n \t\t\t\t\t\t--- EQUIPMENT LIST ---");
+        System.out.println(
+                "ID: " + getId() +
+                        " | Name: " + getName() +
+                        " | Category: " + getCategory() +
+                        " | Qty: " + getQuantity() +
+                        " | Condition: " + getCondition()
+        );
+    }
 }

c55042aa6744446440fc3a292191ca280fa00f56
tnchzrvgr <vergaratoni783@gmail.com>
2025-11-21 20:46:07 +0800

    Update Equipment.java

--- Equipment.java
+++ Equipment.java
@@ -1,4 +1,4 @@
-public class Equipment {
+public abstract class Equipment {
     private int id;
     private String name;
     private String category;
@@ -15,8 +15,24 @@ public class Equipment {
 
     public int getId() { return id; }
     public String getName() { return name; }
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getCategory() { return category; }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
     public int getQuantity() { return quantity; }
-    public void setQuantity(int quantity) { this.quantity = quantity; }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
     public String getCondition() { return condition; }
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public abstract void displayInfo();
 }
